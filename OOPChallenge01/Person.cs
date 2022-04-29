05f716cb5259b86d405bfcf143950d02de7ee0f3
hufesouza <hufesouza@gmail.com>
2022-04-29 14:04:25 +0100

    First commit

---
+++ OOPChallenge01/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPChallenge01
+{
+    internal class Person
+    {
+        //Defining Property
+        public string Name { get; set; }
+        
+        //Default constructor
+        public Person()
+        {
+            Name = "Jonas";
+        }
+
+        //Simple Constructor
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+
+        //Override ToString() Method.
+        public override string ToString()
+        {
+            return String.Format($"Hello!! My name is {this.Name}");
+        }
+
+        //Setting descructor
+        ~Person()
+        {
+            Name = string.Empty;
+        }
+    }
+}
---
+++ OOPChallenge01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OOPChallenge01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating an array type string
+            string[] person = new string[3];
+
+            // creating a loop to add names into the array
+            for (int o = 0; o < person.Length; o++)
+            {
+                Console.WriteLine("Please, let me know the name!");
+                person[o] = Console.ReadLine();
+                Console.Clear();
+            }
+            //Creating new Objects
+            Person a = new Person(person[0]);
+            Person b = new Person(person[1]);
+            Person c = new Person(person[2]);
+
+            //Greeting each person in the array.
+            Console.WriteLine(a.ToString());
+            Console.WriteLine(b.ToString());
+            Console.WriteLine(c.ToString());
+        }
+    }
+}
